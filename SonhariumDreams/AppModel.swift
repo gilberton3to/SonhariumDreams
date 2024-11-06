98f10889c52b41ded69f7f6809521ea85eedfd95
Gilberto Neto <gilberytorid@gmail.com>
2024-11-06 11:47:52 -0300

    Initial Commit

---
+++ SonhariumDreams/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  SonhariumDreams
+//
+//  Created by Gilberto Neto on 06/11/24.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ SonhariumDreamsTests/SonhariumDreamsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SonhariumDreamsTests.swift
+//  SonhariumDreamsTests
+//
+//  Created by Gilberto Neto on 06/11/24.
+//
+
+import Testing
+@testable import SonhariumDreams
+
+struct SonhariumDreamsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
